f30884351ad568831e69e5f2eae24b90c7db7e3e
FrancisPog1 <allanbelenisko@gmail.com>
2023-06-28 23:52:53 +0800

    DELETED 11:52

--- resources/views/Academic_head/Admin_Setup/AcadHead_Programs/view_modal.blade.php
+++
@@ -1,7 +0,0 @@
-<section class="content">
-    <form id="viewForm" action="" method="post">
-        <div class="modal fade" id="modal-xl-view">
-            @include('Form_Group.view_form')
-        </div>
-    </form>
-</section>
