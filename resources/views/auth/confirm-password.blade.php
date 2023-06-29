776e0183c31656845a9cc79870403844086b8fd7
FrancisPog1 <allanbelenisko@gmail.com>
2023-06-29 20:35:17 +0800

    Update confirm-password.blade.php

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -10,10 +10,8 @@
         <div>
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -25,3 +23,4 @@
         </div>
     </form>
 </x-guest-layout>
+{{-- Francis --}}
