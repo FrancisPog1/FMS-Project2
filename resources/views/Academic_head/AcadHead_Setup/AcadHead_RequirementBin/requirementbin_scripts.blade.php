c6d4483a23746966710ff59474aa74f628bd827a
FrancisPog1 <allanbelenisko@gmail.com>
2023-07-25 01:56:00 +0800

    I deleted all the datatables files so that I can uninstall the datatable package

--- resources/views/Academic_head/AcadHead_Setup/AcadHead_RequirementBin/requirementbin_scripts.blade.php
+++ resources/views/Academic_head/AcadHead_Setup/AcadHead_RequirementBin/requirementbin_scripts.blade.php
@@ -345,12 +345,7 @@
 
 <script>
 
-import DataTable from 'datatables.net-dt';
-import 'datatables.net-responsive-dt';
 
-let table = new DataTable('#myTable', {
-    responsive: true
-});
 
 </script>
 
